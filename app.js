a64966d23efe8ba06ae3c1beabe42f3cdf0369d6
Dazamova <azamovadi@yandex.com>
2023-04-30 18:28:44 +0800

    Бэкенд диплома

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/error-handler');
 
-const { PORT = 3002, BASE_PATH = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 3000, BASE_PATH = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 const app = express();
 
 mongoose.connect(BASE_PATH, {}) // Подключаемся к серверу mongo
@@ -22,12 +22,12 @@ mongoose.connect(BASE_PATH, {}) // Подключаемся к серверу mo
 
 const corsOptions = {
   origin: [
-    // 'https://localhost:3000',
-    // 'http://localhost:3000',
-    // 'http://mesto-travel.nomoredomains.monster',
-    // 'https://mesto-travel.nomoredomains.monster',
-    // 'http://api.mesto-travel.nomoredomains.monster',
-    // 'https://api.mesto-travel.nomoredomains.monster',
+    'https://localhost:3000',
+    'http://localhost:3000',
+    'http://movies-scout.nomoredomains.monster',
+    'https://movies-scout.nomoredomains.monster',
+    'http://api.movies-scout.nomoredomains.monster',
+    'https://api.movies-scout.nomoredomains.monster',
   ],
   credentials: true,
 };
